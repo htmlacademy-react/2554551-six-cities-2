de895d1257ff64acf26ccc5106270debfc09a1e3
Anastasiya <arsaidaz10@mts.ru>
2024-10-01 23:53:45 +0300

    Add components

--- src/components/main-empty/main-empty.tsx
+++ src/components/main-empty/main-empty.tsx
@@ -1,9 +1,6 @@
 /* eslint-disable arrow-body-style */
 
-import { cards } from '../../data/cardsData';
-import Card from '../card/card';
-
-const Main = () => {
+const MainEmpty = () => {
   return (
     <div className="page page--gray page--main">
       <header className="header">
@@ -44,7 +41,8 @@ const Main = () => {
           </div>
         </div>
       </header>
-      <main className="page__main page__main--index">
+
+      <main className="page__main page__main--index page__main--index-empty">
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
           <section className="locations container">
@@ -65,7 +63,7 @@ const Main = () => {
                 </a>
               </li>
               <li className="locations__item">
-                <a className="locations__item-link tabs__item tabs__item--active">
+                <a className="locations__item-link tabs__item">
                   <span>Amsterdam</span>
                 </a>
               </li>
@@ -75,7 +73,10 @@ const Main = () => {
                 </a>
               </li>
               <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
+                <a
+                  className="locations__item-link tabs__item tabs__item--active"
+                  href="#"
+                >
                   <span>Dusseldorf</span>
                 </a>
               </li>
@@ -83,54 +84,17 @@ const Main = () => {
           </section>
         </div>
         <div className="cities">
-          <div className="cities__places-container container">
-            <section className="cities__places places">
-              <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">312 places to stay in Amsterdam</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                  <svg className="places__sorting-arrow" width="7" height="4">
-                    <use xlinkHref="#icon-arrow-select"></use>
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li
-                    className="places__option places__option--active"
-                    tabIndex={0}
-                  >
-                    Popular
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: low to high
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: high to low
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Top rated first
-                  </li>
-                </ul>
-              </form>
-              <div className="cities__places-list places__list tabs__content">
-                {cards.map((card) => (
-                  <Card
-                    key={crypto.randomUUID()}
-                    isPremium={card.isPremium}
-                    imgPath={card.imgPath}
-                    price={card.price}
-                    inBookmarks={card.inBookmarks}
-                    rating={card.rating}
-                    placeName={card.placeName}
-                    placeType={card.placeType}
-                  />
-                ))}
+          <div className="cities__places-container cities__places-container--empty container">
+            <section className="cities__no-places">
+              <div className="cities__status-wrapper tabs__content">
+                <b className="cities__status">No places to stay available</b>
+                <p className="cities__status-description">
+                  We could not find any property available at the moment in
+                  Dusseldorf
+                </p>
               </div>
             </section>
-            <div className="cities__right-section">
-              <section className="cities__map map"></section>
-            </div>
+            <div className="cities__right-section"></div>
           </div>
         </div>
       </main>
@@ -138,4 +102,4 @@ const Main = () => {
   );
 };
 
-export default Main;
+export default MainEmpty;
