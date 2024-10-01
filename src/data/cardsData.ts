4fbc9931920ee0a1997b6ec5ea75f9c2b2585a8c
Anastasiya <iinokayo@mail.ru>
2024-10-01 23:07:34 +0300

    Render app in index

---
+++ src/data/cardsData.ts
@@ -0,0 +1,49 @@
+import { SingleCard } from '../types';
+
+export const cards: SingleCard[] = [
+  {
+    isPremium: true,
+    imgPath: 'img/apartment-01.jpg',
+    price: 120,
+    inBookmarks: false,
+    rating: 4,
+    placeName: 'Beautiful &amp; luxurious apartment at great location',
+    placeType: 'Apartment',
+  },
+  {
+    isPremium: false,
+    imgPath: 'img/room.jpg',
+    price: 80,
+    inBookmarks: true,
+    rating: 4,
+    placeName: 'Wood and stone place',
+    placeType: 'Private room',
+  },
+  {
+    isPremium: false,
+    imgPath: 'img/apartment-02.jpg',
+    price: 132,
+    inBookmarks: false,
+    rating: 4,
+    placeName: 'Canal View Prinsengracht',
+    placeType: 'Apartment',
+  },
+  {
+    isPremium: true,
+    imgPath: 'img/apartment-03.jpg',
+    price: 180,
+    inBookmarks: false,
+    rating: 5,
+    placeName: 'Nice, cozy, warm big bed apartment',
+    placeType: 'Apartment',
+  },
+  {
+    isPremium: false,
+    imgPath: 'img/room.jpg',
+    price: 80,
+    inBookmarks: true,
+    rating: 4,
+    placeName: 'Wood and stone place',
+    placeType: 'Private room',
+  },
+];
