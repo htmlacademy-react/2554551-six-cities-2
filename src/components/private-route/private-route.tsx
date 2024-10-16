266035f681a3ca2525f2a34a48582d44b19f35a1
Anastasiya <arsaidaz10@mts.ru>
2024-10-16 19:16:42 +0300

    Fix props type

--- src/components/private-route/private-route.tsx
+++ src/components/private-route/private-route.tsx
@@ -1,14 +1,21 @@
-import { PropsWithChildren } from 'react';
+import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { AppRoute, AuthorizationStatus } from '../../const';
 
-type Props = PropsWithChildren<{ authorizationStatus: AuthorizationStatus }>;
+// Сборка не проходит, если испльзовать PropsWithChildren и не указать тип Navigate
+// 'PrivateRoute' cannot be used as a JSX component.
+// Its return type 'string | number | boolean | Element | Iterable<ReactNode> | null | undefined' is not a valid JSX element.
+// Type 'undefined' is not assignable to type 'Element | null'.
+
+// type Props = PropsWithChildren<{ authorizationStatus: AuthorizationStatus }>;
+type Props = {
+  authorizationStatus: AuthorizationStatus;
+  children: React.JSX.Element;
+};
 
 const PrivateRoute = ({ authorizationStatus, children }: Props) =>
-  authorizationStatus === AuthorizationStatus.Auth ? (
-    children
-  ) : (
-    <Navigate to={AppRoute.Login} />
-  );
+  authorizationStatus === AuthorizationStatus.Auth
+    ? children
+    : ((<Navigate to={AppRoute.Login} />) as React.JSX.Element);
 
 export default PrivateRoute;
