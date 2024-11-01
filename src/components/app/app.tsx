2f56f6117a79a9660517be95139d073b5bb31f0e
Anastasiya <iinokayo@mail.ru>
2024-11-01 19:44:31 +0300

    Add mock data

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable arrow-body-style */
 import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import { AppRoute, AuthorizationStatus } from '../../const';
+import { SingleCard } from '../../lib/types.ts/card';
+import { Favorite } from '../../lib/types.ts/favorite';
 import Main from '../../pages/main/main';
 import Login from '../../pages/login/login';
 import Favorites from '../../pages/favorites/favorites';
@@ -8,17 +10,19 @@ import Offer from '../../pages/offer/offer';
 import NotFound from '../../pages/not-found/not-found';
 import PrivateRoute from '../private-route/private-route';
 
-const App = () => {
+type Props = { offers: SingleCard[]; favorites: Favorite[] };
+
+const App = ({ offers, favorites }: Props) => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={AppRoute.Main} element={<Main />} />
+        <Route path={AppRoute.Main} element={<Main offers={offers} />} />
         <Route path={AppRoute.Login} element={<Login />} />
         <Route
           path={AppRoute.Favorites}
           element={
             <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
-              <Favorites />
+              <Favorites favorites={favorites} />
             </PrivateRoute>
           }
         />
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
+import { OFFERS } from './mocks/offers';
+import { FAVORITES } from './mocks/favorites';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -8,6 +10,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App offers={OFFERS} favorites={FAVORITES} />
   </React.StrictMode>
 );
