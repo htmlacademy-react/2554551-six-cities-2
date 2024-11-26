1f14dbb7ae13cbfc8ed698727a69e21443f99c5f
Anastasiya <arsaidaz10@mts.ru>
2024-11-26 16:35:19 +0300

    Add props

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -3,6 +3,8 @@ import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import { AppRoute, AuthorizationStatus } from '../../const';
 import { SingleCard } from '../../lib/types.ts/card';
 import { Favorite } from '../../lib/types.ts/favorite';
+import { City } from '../../lib/types.ts/city';
+import { Point } from '../../lib/types.ts/point';
 import Main from '../../pages/main/main';
 import Login from '../../pages/login/login';
 import Favorites from '../../pages/favorites/favorites';
@@ -10,13 +12,21 @@ import Offer from '../../pages/offer/offer';
 import NotFound from '../../pages/not-found/not-found';
 import PrivateRoute from '../private-route/private-route';
 
-type Props = { offers: SingleCard[]; favorites: Favorite[] };
+type Props = {
+  offers: SingleCard[];
+  favorites: Favorite[];
+  city: City;
+  points: Point[];
+};
 
-const App = ({ offers, favorites }: Props) => {
+const App = ({ offers, favorites, city, points }: Props) => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={AppRoute.Main} element={<Main offers={offers} />} />
+        <Route
+          path={AppRoute.Main}
+          element={<Main offers={offers} city={city} points={points} />}
+        />
         <Route path={AppRoute.Login} element={<Login />} />
         <Route
           path={AppRoute.Favorites}
