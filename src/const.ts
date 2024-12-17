ca843b9936d41d249d6b8f293ab72c968bd5d801
Anastasiya <arsaidaz10@mts.ru>
2024-12-17 20:02:34 +0300

    Update map markers

--- src/const.ts
+++ src/const.ts
@@ -21,10 +21,12 @@ export enum PlacesSortingName {
 }
 
 export const URL_MARKER_DEFAULT =
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
+  'https://img.icons8.com/ios-filled/50/228BE6/marker.png';
+// 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
 
 export const URL_MARKER_CURRENT =
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
+  'https://img.icons8.com/ios-filled/50/FAB005/marker.png';
+// 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
 
 export const CARD_OPTIONS: Record<CardType, Record<CardOptions, string>> = {
   offer: {
