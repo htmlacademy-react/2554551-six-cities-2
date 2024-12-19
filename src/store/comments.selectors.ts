8c35ef0d1e237e869d790330680e30a46fff6cdb
Anastasiya <arsaidaz10@mts.ru>
2024-12-20 01:57:35 +0300

    Limit comments

--- src/store/comments.selectors.ts
+++ src/store/comments.selectors.ts
@@ -6,9 +6,9 @@ const selectAllComments = (state: RootState) => state;
 const getSortedComments = (state: RootState) => {
   const commentsCopy = [...state.comments];
 
-  return commentsCopy.sort(
-    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
-  );
+  return commentsCopy
+    .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+    .slice(0, 10);
 };
 
 export const selectSortedComments = createSelector(
